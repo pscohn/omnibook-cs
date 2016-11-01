eae21e865eeb5928a67f0031e396966e7087a9f9
Paul Cohn <pscohn@gmail.com>
2016-10-31 23:50:37 -0400

    auth fixes

--- src/bg/background.js
+++ src/bg/background.js
@@ -1,5 +1,5 @@
 var token = null;
-// var apiBase = 'http://foo.com:8000';
+//var apiBase = 'http://foo.com:8000';
 var apiBase = 'https://omnibook.co';
 var apiUrl = apiBase + '/entry/';
 var authUrl = apiBase + '/login';
@@ -13,43 +13,37 @@ var notifications = {
       chrome.tabs.create({ url: authUrl });
       chrome.notifications.clear('auth');
     }
-  }
-}
-
-function authenticate() {
-  chrome.storage.sync.get('omnitoken', function(data) {
-    token = data.token;
-    console.log('auth token', token)
-    if (!token) {
-      notifyAuth();
-    }
-  });
-}
-
-function notifySaved() {
-  chrome.notifications.create('saved', {
+  },
+  'saved': {
     type: 'basic',
     iconUrl: 'icons/icon48.png',
     title: 'Tab saved!',
     message: 'Your tab has been saved.'
-  });
+  },
 }
 
-function notifyAuth() {
-  var authNotification = Object.assign({}, notifications.auth);
-  delete authNotification.onClick;
-  chrome.notifications.create('auth', authNotification);
+function notify(key) {
+  var notification = Object.assign({}, notifications[key]);
+  delete notification.onClick;
+  chrome.notifications.create(key, notification);
 }
 
-function getToken() {
+function authenticate() {
   chrome.storage.sync.get('omnitoken', function(data) {
-    console.log('get', data)
-    return data.token;
+    token = data.omnitoken;
+    console.log('auth token', token)
+    if (!token) {
+      notify('auth');
+    }
   });
 }
 
 function saveTab(title, url) {
   chrome.storage.sync.get('omnitoken', function(data) {
+    if (!data.omnitoken) {
+      return notify('auth');
+    }
+
     console.log('get', data.omnitoken)
     var xhr = new XMLHttpRequest();
     xhr.open('POST', apiUrl, true);
@@ -57,7 +51,7 @@ function saveTab(title, url) {
     xhr.onreadystatechange = function() {
       if (xhr.readyState === 4) {
         console.log(xhr.responseText);
-        notifySaved();
+        notify('saved');
       }
     }
     xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
